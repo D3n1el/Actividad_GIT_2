f2a29e5afd6b51ff03af67674fd31887584642e3
D3n1el <dani.alcaraz.p@gmail.com>
2024-12-16 19:55:58 +0100

    Implementación de la matriz
    Se ha creado el array, y los bucles para generar números aleatorios en cada posición del array.

--- src/Actividad_GIT_2/MatrizApp.java
+++ src/Actividad_GIT_2/MatrizApp.java
@@ -1,10 +1,21 @@
 package Actividad_GIT_2;
 
+import java.util.Random;
+
 public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//matriz-inicial: Para generar y mostrar la matriz de números aleatorios.
+		Random r = new Random();
+		
+		int[][] matriz = new int[5][5];
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = r.nextInt(100) + 1;
+			}
+		}	
 	}
-
 }
