de8bee30ad1fdb777b5a28d90e5890f47f00b21e
oriol-valentines <oriolvalentinesmiro@gmail.com>
2024-12-17 19:15:19 +0100

    commit canvios
    Esta parte ya esta terminada, ya que se ha solucionado el problema que no mostraba ninguna posición que pasaba de la fila en la posición 0 y la columna en la posición 0, se ha implementado un boolean por si aciertas el numero o por si lo fallas, también se ha implementado systems.print para un mejor efecto visual.

--- src/Actividad_GIT_2/MatrizApp.java
+++ src/Actividad_GIT_2/MatrizApp.java
@@ -8,24 +8,29 @@ public class MatrizApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
-		//MI PARTE
 		//Buscar un número y mostrar su posición.
 		Scanner x = new Scanner(System.in);
 		System.out.print("¿Que numero desea buscar? ");
 		int numAbuscar = x.nextInt();
 		
+		boolean encontrado = false;
+		
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 				if(matriz[i][j] == numAbuscar) {
-					System.out.print(numAbuscar);
-				}else {
-					System.out.print("Este numero no esta en la matriz");
-					return;
+					System.out.println("el num " + numAbuscar);
+					System.out.println("Fila " + (i+1));
+					System.out.println("columna " + (j+1));
+					System.out.println("__________________");
+					System.out.println();
+					encontrado = true;
 				}
 			}
-			
 		}
+		if(!encontrado) {
+			System.out.print("Este numero no esta en la matriz");
+		}
+		
 		
 	}
 
