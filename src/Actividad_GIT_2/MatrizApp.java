1e567d0c436a184b579334651f52e05cf9821c90
oriol-valentines <oriolvalentinesmiro@gmail.com>
2024-12-17 18:51:03 +0100

    Commit rama Oriol_Valentines
    Se ha creado una entrada de info para ver si el numero que quiero esta en el array.

--- src/Actividad_GIT_2/MatrizApp.java
+++ src/Actividad_GIT_2/MatrizApp.java
@@ -1,10 +1,32 @@
 package Actividad_GIT_2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		//MI PARTE
+		//Buscar un número y mostrar su posición.
+		Scanner x = new Scanner(System.in);
+		System.out.print("¿Que numero desea buscar? ");
+		int numAbuscar = x.nextInt();
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if(matriz[i][j] == numAbuscar) {
+					System.out.print(numAbuscar);
+				}else {
+					System.out.print("Este numero no esta en la matriz");
+					return;
+				}
+			}
+			
+		}
+		
 	}
 
 }
